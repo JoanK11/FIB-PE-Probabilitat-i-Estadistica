a6921b6873985c2858ebc3519933c6e0a11a7e71
Joan Caballero <70658820+JoanK11@users.noreply.github.com>
2021-10-26 22:37:22 +0200

    v2

--- e-status/B3/Un_de_cada_milio_de_sectors.R
+++ e-status/B3/Un_de_cada_milio_de_sectors.R
@@ -1,37 +1,37 @@
-# Un de cada miliÛ de sectors
-# El proveidor de discs durs de l'any passat comercialitza ara un model de {tamany = 325} GB, i estem analitzant si ens convÈ. Aquest fabricant ha fet proves per quantificar l'apariciÛ de sectors defectuosos, que no sol ser un problema mentre sigui un nombre petit, i ha determinat que la probabilitat que no s'observi cap defecte en un disc tipus de 100 GB durant un any de funcionament Ès {prob = 0.1653}.
+# Un de cada mili√≥ de sectors
+# El proveidor de discs durs de l'any passat comercialitza ara un model de {tamany = 325} GB, i estem analitzant si ens conv√©. Aquest fabricant ha fet proves per quantificar l'aparici√≥ de sectors defectuosos, que no sol ser un problema mentre sigui un nombre petit, i ha determinat que la probabilitat que no s'observi cap defecte en un disc tipus de 100 GB durant un any de funcionament √©s {prob = 0.1653}.
 tamany = 325;
 prob = 0.1653;
 
-# Pregunta 1: Una d'aquestes distribucions correspon al nombre de sectors defectuosos d'un disc de 325 GB en un any (es mostren els valors corresponents al 99% central). Selecciona la distribuciÛ correcta (1, 2 o 3).
+# Pregunta 1: Una d'aquestes distribucions correspon al nombre de sectors defectuosos d'un disc de 325 GB en un any (es mostren els valors corresponents al 99% central). Selecciona la distribuci√≥ correcta (1, 2 o 3).
 lambda = -log(prob)*tamany/100;
 x1 = floor(lambda-1); x2 = floor(lambda);
 x3 = floor(lambda+1); x4 = floor(lambda+2);
 q1 = dpois(x1, lambda); q2 = dpois(x2, lambda);
 q3 = dpois(x3, lambda); q4 = dpois(x4, lambda);
 cat("En x =", x1, ": p =", q1, "\nEn x =", x2, ": p =", q2, "\nEn x =", x3, ": p =", q3, "\nEn x =", x4, ": p =", q4);
-# El resultat ser‡ la gr‡fica que tingui en els valors x1, x2, x3 i x4 les seves probabilitats corresponents.
+# El resultat ser√† la gr√†fica que tingui en els valors x1, x2, x3 i x4 les seves probabilitats corresponents.
 
-# Pregunta 2: Quin seria el nombre esperat de defectes en el disc que es comercialitza, per a un hipotËtic periode de {anys = 4} anys i {mesos = 6} mesos d'activitat?
+# Pregunta 2: Quin seria el nombre esperat de defectes en el disc que es comercialitza, per a un hipot√®tic periode de {anys = 4} anys i {mesos = 6} mesos d'activitat?
 anys = 4; mesos = 6;
 temps = anys + mesos/12;
 pregunta2 <-  lambda*temps;
 pregunta2;
 
-# Pregunta 3: Segurament Ès preferible tenir una mesura de quin Ès el risc en una situaciÛ mÈs extrema. Per exemple, saber quin Ès el m‡xim nombre previst de sectors defectuosos, amb un error del {error = 5}%, per discos amb {anys = 4} anys i {mesos = 6} mesos d'antiguitat. Respon amb un nombre enter, arrodonit per excÈs.
-error = 5;
+# Pregunta 3: Segurament √©s preferible tenir una mesura de quin √©s el risc en una situaci√≥ m√©s extrema. Per exemple, saber quin √©s el m√†xim nombre previst de sectors defectuosos, amb un error del {error = 5%}, per discos amb {anys = 4} anys i {mesos = 6} mesos d'antiguitat. Respon amb un nombre enter, arrodonit per exc√©s.
+error = 0.05;
 temps = anys + mesos/12;
-pregunta3 <- ceiling(qnorm(1-error/100, lambda*temps, sqrt(lambda*temps)));
+pregunta3 <- ceiling(qnorm(1-error, lambda*temps, sqrt(lambda*temps)));
 pregunta3;
 
-# Pregunta 4: La garantia dels discs estableix que es reemplaÁaran de franc, i per un ˙nic cop, els discos avariats dins del perÌode de garantia, entenent-se per avariat un disc amb mÈs de 'n' sectors defectuosos. Calculeu quin ha de ser el valor de 'n' per tal que, en promig, nomÈs calgui substituir, com a m‡xim, un 1% dels discos al llarg d'un perÌode de garantia de {anys = 3} anys.
+# Pregunta 4: La garantia dels discs estableix que es reempla√ßaran de franc, i per un √∫nic cop, els discos avariats dins del per√≠ode de garantia, entenent-se per avariat un disc amb m√©s de 'n' sectors defectuosos. Calculeu quin ha de ser el valor de 'n' per tal que, en promig, nom√©s calgui substituir, com a m√†xim, un 1% dels discos al llarg d'un per√≠ode de garantia de {anys = 3} anys.
 anys = 3;
 pregunta4 <- ceiling(qnorm(0.99, lambda*anys, sqrt(lambda*anys)));
 pregunta4;
 
-# Pregunta 5: En lloc d'una garantia de {anys1 = 3} anys, s'ofereix una alternativa: {anys2 = 2.5} anys, tenint en compte que s'enten com a disc avariat si presenta {n = 21} sectors defectuosos com a mÌnim. Si un client agafa aquest garantia,
-    # quina Ès la probabilitat que li hagin de reemplaÁar el disc?
-    # …s preferible aquesta garantia, des del punt de vista del client?
+# Pregunta 5: En lloc d'una garantia de {anys1 = 3} anys, s'ofereix una alternativa: {anys2 = 2.5} anys, tenint en compte que s'enten com a disc avariat si presenta {n = 21} sectors defectuosos com a m√≠nim. Si un client agafa aquest garantia,
+    # quina √©s la probabilitat que li hagin de reempla√ßar el disc?
+    # √âs preferible aquesta garantia, des del punt de vista del client?
 anys1 = 3; anys2 = 2.5;
 n = 21;
 p1 <- pnorm(n, lambda*anys1, sqrt(lambda*anys1));
@@ -40,7 +40,7 @@ millor = "S";
 if (p1 >= p2) millor = "N";
 print(paste(1 - p2, millor));
 
-# Pregunta 6: Calculeu el valor esperat del cost per substituciÛ en garantia per disc venut, si el perÌode de garantia Ès de {anys = 3} anys, 'n' = {n = 24} i el cost d'un disc Ès de {preu = 1542} euros.
+# Pregunta 6: Calculeu el valor esperat del cost per substituci√≥ en garantia per disc venut, si el per√≠ode de garantia √©s de {anys = 3} anys, 'n' = {n = 24} i el cost d'un disc √©s de {preu = 1542} euros.
 anys = 3;
 n = 24;
 preu = 1542;
